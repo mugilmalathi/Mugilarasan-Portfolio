30323022f28f5eb4bb06d3b911627fe270bf30fa
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-03-18 00:13:40 +0530

    portfolio

---
+++ fw14_299/components/index.js
@@ -0,0 +1,62 @@
+function showResume(){
+
+    window.open('https://drive.google.com/file/d/17aD0InJfukHQuwTmQ5CdGY2RS4OJ0aff/view?usp=sharing');
+}
+
+function github(){
+    
+    window.open('https://github.com/mugilmalathi');
+}
+
+function linkedin(){
+    
+    window.open('https://www.linkedin.com/in/mugilarasan-s-3992311a4/');
+}
+function twitter(){
+    
+    window.open('https://twitter.com/Mugil28');
+}
+
+function phone(){
+    document.getElementById('show-social').innerText = "+91 6374488232"
+}
+
+function contactTwitter(){
+    window.open('https://twitter.com/Mugil28')
+}
+
+function contactLinkedIn(){
+    window.open('https://www.linkedin.com/in/mugilarasan-s-3992311a4/')
+}
+
+function githubDunzo(){
+    window.open('https://github.com/mugilmalathi/Dunzo_Clone')
+}
+
+function githubDerm(){
+    window.open('https://github.com/mugilmalathi/DermStore')
+}
+
+function liveDerm(){
+    window.open('https://practical-curran-e48460.netlify.app/')
+}
+
+function liveDunzo(){
+    window.open('https://sid283.github.io/dunzo_clone/homepage/index.html')
+}
+
+function contact(){
+    document.getElementById('social-show').innerText="+91 6374488232"
+}
+
+function socialLocation(){
+    document.getElementById('social-show').innerText="Dharmapuri, Tamilnadu"
+}
+
+function gmail(){
+    window.open('https://mail.google.com/mail/u/0/#inbox')
+}
+
+document.getElementById('nav-contact').addEventListener('scroll', function(){
+    let contact = window.screenY;
+})
